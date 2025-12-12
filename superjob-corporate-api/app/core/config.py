32050429ae7e6582a57dca4dbd8c4806b8843bb2
ikhwanand <aldirizaldy977@gmail.com>
2025-12-12 18:10:45 +0700

    feat: include user name in log activities API response

--- superjob-corporate-api/app/core/config.py
+++ superjob-corporate-api/app/core/config.py
@@ -2,11 +2,15 @@ import os
 from dotenv import load_dotenv
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path 
+
+env_path = Path(__file__).parent.parent.parent / ".env"
 
 # Only load .env in development
-if os.getenv("RENDER") is None:  # Not running on Render
-    load_dotenv()
+# if os.getenv("RENDER") is None:  # Not running on Render
+#     load_dotenv(dotenv_path=env_path)
 
+load_dotenv(dotenv_path=env_path)
 # load_dotenv()
 
 class Settings:
@@ -18,7 +22,7 @@ class Settings:
     # Database Configuration (gunakan environment variables dari Render)
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
     DB_PORT: str = os.getenv("DB_PORT", "5432")
-    DB_NAME: str = os.getenv("DB_NAME", "super_job_db")
+    DB_NAME: str = os.getenv("DB_NAME", "corporate")
     DB_USER: str = os.getenv("DB_USER", "postgres")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "")
     

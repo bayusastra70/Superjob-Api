e3db7680480b69fec0940dc28871c17dbb5e31dd
amin50 <muhammadam_ojt@solvera.id>
2025-12-14 11:09:12 +0700

    empty dev

--- superjob-corporate-api/app/db/base.py
+++
@@ -1,9 +0,0 @@
-# from sqlalchemy.orm import DeclarativeBase
-
-
-# class Base(DeclarativeBase):
-#     pass
-
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
--- superjob-talent-api/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.company import Company
-from app.models.company_review import CompanyReview
-from app.models.user import User
-
-__all__ = ["Company", "CompanyReview", "User"]
